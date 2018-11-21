0d56da9d8173fa366af63ec738e51b1706d777e2
mswift42 <martin.haesler@gmail.com>
2018-11-21 12:48:36 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ckfl
+
+ckfl
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ android/app/src/main/java/com/mswift42/ckfl/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mswift42.ckfl;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
