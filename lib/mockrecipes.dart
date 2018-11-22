e3435109a99d1e3d069cfac2460d0071990f8c4c
mswift42 <martin.haesler@gmail.com>
2018-11-22 06:20:51 +0000

    add mockrecipes.

---
+++ lib/mockrecipes.dart
@@ -0,0 +1,31 @@
+import 'package:ckfl/Recipe.dart';
+
+List<Recipe> recipes = [
+  Recipe(
+    "Grüne Bohnen im Speckmantel",
+    "Bohnen waschen und die Spitzen abschneiden. Bohnenkraut, Knoblauch, zerdrückte Pfefferkörner und Salz mit Öl kurz anrösten. 2 Lite...",
+    "https://www.chefkoch.de/rezepte/563451154612271/Gruene-Bohnen-im-Speckmantel.html",
+    "https://static.chefkoch-cdn.de/rs/bilder/56345/gruene-bohnen-im-speckmantel-1124631-150x150.jpg",
+    "4.49",
+    "simpel",
+    "30 min.",
+  ),
+  Recipe(
+   "Grüne Bohnen",
+    "Variante 1: Die Bohnen putzen. Zwiebeln und Knoblauch klein schneiden und in etwas Butter oder Margarine anbraten. Die Bohnen dazu...",
+    "https://www.chefkoch.de/rezepte/3166211471333987/Gruene-Bohnen.html",
+    "https://static.chefkoch-cdn.de/rs/bilder/316621/gruene-bohnen-938192-150x150.jpg",
+    "4.36",
+    "simpel",
+    "10 min.",
+  ),
+  Recipe(
+    "Schupfnudel - Bohnen - Pfanne",
+    "Pfannengericht mit Bohnen, Schinken, Schupfnudeln und Crème fraiche",
+    "https://www.chefkoch.de/rezepte/1171381223217983/Schupfnudel-Bohnen-Pfanne.html",
+    "https://static.chefkoch-cdn.de/rs/bilder/117138/schupfnudel-bohnen-pfanne-1156413-150x150.jpg",
+    "4.37",
+    "normal",
+    "30 min.",
+  ),
+];
