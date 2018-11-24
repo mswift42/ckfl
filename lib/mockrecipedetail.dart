4c9d2bf1ae5d3ccd70d5a979e2094fee60348a4e
mswift42 <martin.haesler@gmail.com>
2018-11-24 10:26:48 +0000

    add missing ingredients field.

--- lib/mockrecipedetail.dart
+++ lib/mockrecipedetail.dart
@@ -8,6 +8,7 @@ var grueneImSpeckMantel = RecipeDetail(
   cookingtime: "ca. 15 Min.",
   thumbnail:
       "https://static.chefkoch-cdn.de/ck.de/rezepte/56/56345/1124631-420x280-fix-gruene-bohnen-im-speckmantel.jpg",
+  ingredients: [RecipeIngredient("800\u00a0g", "Bohnen, frische")],
   method:
       "Bohnen waschen und die Spitzen abschneiden.\nBohnenkraut, Knoblauch, zerdrückte Pfefferkörner und Salz mit Öl kurz anrösten. 2 Liter Wasser zugießen, 10 Min. kochen, durchsieben. Diese Brühe aufkochen und die Bohnen in 3 Portionen nacheinander sprudelnd garen. Schnell in kaltem Wasser abkühlen, in einem Tuch abtrocknen.\n\nBohnen in Bacon einwickeln. Butter in einer feuerfesten Form erhitzen, die Bohnen reingeben (mit der Specknaht nach unten) und zugedeckt im Ofen bei 180 °C - 200 °C erhitzen (ca. 5 Minuten), dabei einmal wenden.",
 );
