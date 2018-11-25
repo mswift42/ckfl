fd1f9d314df442e053ddac5a7df0f81416e56360
mswift42 <martin.haesler@gmail.com>
2018-11-25 10:44:37 +0000

    add file RecipeGridWidget.

---
+++ lib/widgets/recipe_grid_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ckfl/Recipe.dart' show Recipe;
+
+class RecipeGrid extends StatelessWidget {
+  final List<Recipe> _recipes;
+  RecipeGrid(this._recipes);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
