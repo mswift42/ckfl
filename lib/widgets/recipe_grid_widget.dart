b50475d57a43381a54f22870cec2483d3ea29a7c
mswift42 <martin.haesler@gmail.com>
2018-11-25 11:41:27 +0000

    add class _RecipeSearchItemTitle.

--- lib/widgets/recipe_grid_widget.dart
+++ lib/widgets/recipe_grid_widget.dart
@@ -3,23 +3,43 @@ import 'package:ckfl/Recipe.dart' show Recipe;
 
 class RecipeGrid extends StatelessWidget {
   final List<Recipe> _recipes;
+
   RecipeGrid(this._recipes);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: GridView.extent(
           maxCrossAxisExtent: 260.0,
-        children: _recipes.map((i) => _RecipeSearchItem(i)).toList()
-      ),
-      
-
+          children: _recipes.map((i) => _RecipeSearchItem(i)).toList()),
     );
   }
 }
 
 class _RecipeSearchItem extends StatelessWidget {
   final Recipe _recipe;
+
   _RecipeSearchItem(this._recipe);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridTile(
+        child: Image.network(_recipe.thumbnail,
+        fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black26,
+          title: Text(_recipe.title),
+        ),
+
+      ),
+    );
+  }
+}
+class _RecipeSearchItemTitle extends StatelessWidget {
+  final String _title;
+  _RecipeSearchItemTitle(this._title);
   @override
   Widget build(BuildContext context) {
     return Container();
