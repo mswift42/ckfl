3d4f593ca1badcdd742f7c3abc4c35748710c01c
mswift42 <martin.haesler@gmail.com>
2018-11-27 06:11:35 +0000

    add stful widget recipeviewer.

--- lib/widgets/recipe_grid_widget.dart
+++ lib/widgets/recipe_grid_widget.dart
@@ -65,4 +65,15 @@ class _RecipeSearchItemRating extends StatelessWidget {
   }
 }
 
+class RecipeViewer extends StatefulWidget {
+  @override
+  _RecipeViewerState createState() => _RecipeViewerState();
+}
+
+class _RecipeViewerState extends State<RecipeViewer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
