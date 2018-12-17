857b708daf43c61da2f692e9add46ad093bc390d
mswift42 <martin.haesler@gmail.com>
2018-12-17 14:12:53 +0000

    set primary / accent colors.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CK',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,10 +20,11 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.green[500],
+        accentColor: Colors.lime[500],
+        primarySwatch: Colors.green,
       ),
       home: RecipeGrid(recipes: mockresultlist),
     );
   }
 }
-
