f2778d8d317a0fb2ffc692f80f4dbb5c498fec31
mswift42 <martin.haesler@gmail.com>
2018-12-18 14:48:51 +0000

    add listener for textcontroller.

--- lib/widgets/recipe_search_widget.dart
+++ lib/widgets/recipe_search_widget.dart
@@ -6,9 +6,21 @@ class RecipeSearchView extends StatefulWidget {
 }
 
 class _RecipeSearchViewState extends State<RecipeSearchView> {
+  String searchquery = "";
+  final controller = TextEditingController();
+
+  void _setSearchQueryText() {
+    searchquery = controller.text;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_setSearchQueryText);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
-
