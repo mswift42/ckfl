8dfb984694f0c7e0b4d562166e3453fcd3d44f16
mswift42 <martin.haesler@gmail.com>
2018-12-19 06:25:33 +0000

    add scaffold to materialapp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ckfl/widgets/recipe_grid_widget.dart';
 import 'package:ckfl/mockrecipes.dart';
+import 'package:ckfl/widgets/recipe_search_widget.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,22 +10,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CK',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Colors.green[500],
-        accentColor: Colors.lime[500],
-        primarySwatch: Colors.green,
-      ),
-      home: RecipeGrid(recipes: mockresultlist),
-    );
+        title: 'CK',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primaryColor: Colors.green[500],
+          accentColor: Colors.lime[500],
+          primarySwatch: Colors.green,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("CK"),
+          ),
+          body: RecipeSearchView(),
+        )
+        //RecipeGrid(recipes: mockresultlist),
+        );
   }
 }
