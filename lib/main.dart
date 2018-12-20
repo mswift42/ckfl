0bce7daa56062a3863067d3e46ead7a53b408cfe
mswift42 <martin.haesler@gmail.com>
2018-12-20 06:08:19 +0000

    add initial route.

--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'CK',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primaryColor: Colors.green[500],
-          accentColor: Colors.lime[500],
-          primarySwatch: Colors.green,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("CK"),
-          ),
-          body: RecipeSearchView(),
-        )
-        //RecipeGrid(recipes: mockresultlist),
-        );
+      title: 'CK',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primaryColor: Colors.green[500],
+        accentColor: Colors.lime[500],
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => RecipeSearchView(),
+      },
+      //RecipeGrid(recipes: mockresultlist),
+    );
   }
 }
